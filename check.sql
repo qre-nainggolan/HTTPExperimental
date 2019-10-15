e33d1c46f6949f7b9204c19a6e7b27232001a190
Erik Nainggolan <erik.nainggolan@yahoo.com>
2019-10-15 14:28:40 +0700

    Update check.sql

--- check.sql
+++ check.sql
@@ -1,1 +1,4 @@
 SELECT Estate, YEAR(InspectionDate) AS Year, MONTH(InspectionDate) AS Month, DAY(InspectionDate) AS DAY, InspectionDate, Shift, Employee1 AS Petugas1, COUNT(employee1) AS DiperiksaPetugas1, Employee2 AS Petugas2, COUNT(employee2) AS DiperiksaPetugas2 FROM aa_tblcontrol WHERE estate = 'KTZ' AND YEAR(InspectionDate) = '2019' AND Month(InspectionDate) = '10' GROUP BY InspectionDate, Employee1, Employee2 ORDER BY InspectionDate ASC
+
+
+https://www.oracle.com/technetwork/database/windows/utilsoft-096155.html
